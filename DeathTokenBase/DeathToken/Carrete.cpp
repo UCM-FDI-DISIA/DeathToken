ae7a67561b29bebd0e999a657ee0ea10f556cd6e
Lucky_Blocks <157683274+LuckyBlocks28@users.noreply.github.com>
2025-02-17 23:03:18 +0100

    Inicio generación carrete aleatorio
    Inicio de la lógica de desordenamiento del carrete

--- DeathTokenBase/DeathToken/Carrete.cpp
+++ DeathTokenBase/DeathToken/Carrete.cpp
@@ -3,6 +3,11 @@ Carrete::Carrete(PlayState* g,Texture* celdas,Texture* iconos): sceneObject(g, 1
 }
 vector<int> Carrete::vectorAleatorio() {
 	vector<int> vector;
+	for (int i = 0; i < NUM_ELEMS; ++i) {
+		for (int j : frecuencias) {
+			vector.push_back(i);
+		}
+	}
 
 	return vector;
 }
--- DeathTokenBase/DeathToken/Carrete.h
+++ DeathTokenBase/DeathToken/Carrete.h
@@ -3,7 +3,7 @@
 #include <vector>
 class Carrete : public sceneObject {
 private:
-	const int NUM_ELEMS = 18;
+	const int NUM_ELEMS = 21;
 	const vector<int> frecuencias = { 4,3,3,3,2,2,1 };
 	vector<int> resultante;
 	int index;
