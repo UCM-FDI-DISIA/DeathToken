a56a0135c545e3bad18f13b53f5c99e1164fc946
MANK1NG <ivan.ipr.03@gmail.com>
2025-02-21 13:32:40 +0100

    Código de las canicas🔐
    Creado los métodos para que salgan 3 canicas aleatorias y que compruebe si hay alguna apuesta con premio

--- DeathTokenBase/DeathToken/Marbles.cpp
+++ DeathTokenBase/DeathToken/Marbles.cpp
@@ -1,10 +1,57 @@
 #include "Marbles.h"
 #include "Game.h"	
 
-Marbles::Marbles(Game* game) : GameState(game), texture(game->getTexture(MARBLESBACK)) {}
+Marbles::Marbles(Game* game) : GameState(game), texture(game->getTexture(MARBLESBACK)) {
+	marbles = { 0,0,0,0 };
+}
 
 
-void generateMarbles() {
+void Marbles::generateMarbles() {
+	//En un vector voy metiendo aleatoriamente +1, representando ROJO/VERDE/AZUL/AMARILLO
+	marbles = { 0,0,0,0 };
+	for (int i = 0; i > 3; i++) {
+		int color = rand() % 3;
+		marbles[color]++;
+	}
+}
+int  Marbles::checkBets(int moneyBet) {
+	int moneyWin = 0;
+	//Cuando se hagan los botones cada apuesta hecha se metera en un map indicando que apuesta 
+	//hecha en un vector y el multi que da si gana
+
+	for (const auto& betkey : bets) {//Recorre el map
+		const Bet& typeBet = betkey.second;//Pillo la apuesta
+
+		bool won = true;
+		for (int i = 0; i < 4; i++) {
+			if (marbles[i] < typeBet.typeOfBet[i]) { //Si algun numero del vector es mayor a las canicas, se da esa apuesta como perdida
+				won = false;
+			}
+		}
+
+		if (won) {//Si ha ganado esa apuesta se calcula lo ganado
+			int BetMoneyWin = moneyBet * typeBet.multiplier;
+			moneyWin += BetMoneyWin;
+		}
+	}
+	bets.clear();
+	return moneyWin;
+}
+
+void Marbles::startRound() {
+	play = true;
+}
+void Marbles::update() {
+	if (play) {//Cuando se activa el boton de jugar
+		generateMarbles();//Se generar las canicas aleatorias
+		int moneyWin = checkBets(moneyBet);//Comparar canicas con apuesta
+		//Segun la apuesta porX al dinero metido
+		if (moneyWin > 0) {
+			//mostrar mensaje LOLOLOLO HASSSS GANADOOOOO
+			//AMAZING
+		}
+		play = false;
+	}
 }
 
 void Marbles::render() const {
--- DeathTokenBase/DeathToken/Marbles.h
+++ DeathTokenBase/DeathToken/Marbles.h
@@ -3,22 +3,30 @@
 #include "gameState.h"
 #include "vector"
 #include "Texture.h"
+#include <map>
 
 class Marbles : public GameState
 {
+protected:
+	struct Bet {
+		std::vector<int> typeOfBet;
+		int multiplier;
+	};
+	
+	Texture* texture;
+	std::vector<int> marbles;
+	std::map<int, Bet> bets;
+	bool play = false;
+	int moneyBet;
 public:
 	Marbles(Game* game);
-	enum colors { RED, GREEN, BLUE, YELLOW };
-	Texture* texture;
-
-	std::vector<colors> marbles;
-
-
-	void generateMarbles();
 
 	void render() const override;
+	void update() override;
 
-
+	void generateMarbles();
+	int checkBets(int moneyBet);
+	void startRound();
 
 	//Detectar botones y que apuesta hace y con que cantidad de monedas
 	//Metodo play que hace que el juego lanze las 3 bolas de colores con un random
