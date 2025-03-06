4cb87cdd58f64feb02c41275edb263658b149fc7
Janime555 <157594411+Janime-UCM555@users.noreply.github.com>
2025-03-06 16:21:45 +0100

    primera version con posibilidad de hilos

--- DeathTokenBase/DeathToken/Peleas.cpp
+++ DeathTokenBase/DeathToken/Peleas.cpp
@@ -2,11 +2,13 @@
 #include "Game.h"
 #include "BattleManager.h"
 #include "Peleas.h"
+#include <thread>
 
 Peleas::Peleas(Game* game) : GameState(game) , _battleM(new BattleManager()) {
 	if (_battleM->loadFightersFromJSON("peleadores.json") && _battleM->loadMatchupsFromJSON("../DeathToken/matchups.json")) 
 	{
-		_battleM->StartBattle();
+		std::thread BattleThread([this]() {_battleM->StartBattle();});
+		BattleThread.detach();
 	}
 
 }
