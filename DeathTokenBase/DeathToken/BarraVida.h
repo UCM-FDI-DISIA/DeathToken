d4ff50793599780e3bea1c01b2ff7d6d12c8ab7c
MichelHerraizG <micheher@ucm.es>
2025-03-31 22:39:08 +0200

    Barras de Vida en la escena
    Se añadió la funcionalidad completa de las barras de vida y se añadieron a la escena. Ahora la vida que le queda a los luchadores tiene representación gráfica

--- DeathTokenBase/DeathToken/BarraVida.h
+++ DeathTokenBase/DeathToken/BarraVida.h
@@ -4,15 +4,16 @@
 
 class BarraVida : public GameObject {
 public:
-    BarraVida(GameState* estadoJuego, SDL_Renderer* renderizador, int x, int y,
-        int ancho, int alto, int valorMaximo,
-        SDL_Color colorRelleno, SDL_Color colorFondo);
-
-    BarraVida() : GameObject(), renderizador(nullptr), ancho(0), alto(0),
-        valorActual(0), valorMaximo(0),
-        colorRelleno({ 255, 0, 0, 255 }), colorFondo({ 50, 50, 50, 255 }),
-        visible(false) {}
-
+  BarraVida(GameState* estadoJuego,
+            SDL_Renderer* renderizador,
+            int x,
+            int y,
+            int ancho,
+            int alto,
+            int valorMaximo,
+            SDL_Color colorRelleno = { 0, 255, 0, 255 },
+            SDL_Color colorFondo = { 70, 70, 70, 255 },
+            bool esEspejo = false);
 
     virtual void render() const override;
     virtual void update() override {}  
@@ -20,17 +21,24 @@ public:
     void establecerValor(int nuevoValor);
     void establecerPosicion(int x, int y);
     void establecerVisible(bool esVisible);
-
+    void updateColorBasedOnHealth(float currentHealth, float maxHealth);
     int obtenerValor() const;
     bool estaVisible() const;
 
+        const SDL_Color COLOR_VIDA_ALTA = { 0, 255, 0, 255 };     // Verde
+    const SDL_Color COLOR_VIDA_MEDIA = { 255, 255, 0, 255 };  // Amarillo
+    const SDL_Color COLOR_VIDA_BAJA = { 255, 0, 0, 255 };     // Rojo
+    const SDL_Color COLOR_FONDO = { 70, 70, 70, 255 }; 
+
 private:
+
     SDL_Renderer* renderizador;
      SDL_Rect rect; 
     int ancho, alto;
     int valorActual;
     int valorMaximo;
-    SDL_Color colorRelleno;
+    SDL_Color colorRelleno = COLOR_VIDA_ALTA;
     SDL_Color colorFondo;
     bool visible;
+    bool esEspejo;
 };
