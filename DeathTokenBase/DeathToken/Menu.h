e63f7a5339fbce6560b9b4b0a2aa70272cc40054
Ismaorte <ismaorte@ucm.es>
2025-04-04 17:07:47 +0200

    Meter las físicas en esta rama
    He tenido que meter las físicas aquí porque la rama físicas se ha descontrolado

--- DeathTokenBase/DeathToken/Menu.h
+++ DeathTokenBase/DeathToken/Menu.h
@@ -2,8 +2,14 @@
 #include "Texture.h"
 #include "GameState.h"
 #include "Button.h"
+#include "Baccarat.h"
+#include "Slots.h"
+#include "SlotsLocura.h"
+#include "Marbles.h"
+#include "marblesInsanity.h"
 #include "Collision.h"
 #include "SceneObject.h"
+#include "Mesa.h"
 #include <list>
 
 class Player;
@@ -13,17 +19,21 @@ class Menu : public GameState
 	list<sceneObject*> objetos;
 	Player* ghost = nullptr;
 	Texture* texture;
-	Button* baccarat;
-	Button* marbles;
-	Button* fights;
-	Button* slots;
+	Mesa* _baccarat;
+	Mesa* _marbles;
+	Mesa* _fights;
+	Mesa* _slots;
+	vector<SDL_Rect> obstaculos;
+	vector<SDL_Rect> juegos;
+	vector<Mesa*> games;
+
 	HUDLobby* hud;
+	SDL_Rect cambiarColisiones(SDL_Rect);
 public:
 	Menu(Game* game);
-	void gameChanger(GameState* juego);
 	void render() const override;
-	Collision checkCollision(const SDL_Rect& rect, Collision::Target target);
 	void update() override;
 	void handleEvent(const SDL_Event& event);//para colisiones con button
+	vector<SDL_Rect> getLimits() const { return obstaculos; };
 };
 
---
+++ DeathTokenBase/DeathToken/Mesa.cpp
@@ -0,0 +1,41 @@
+#include "checkML.h"
+#include "Mesa.h"
+#include "Game.h"
+
+
+Mesa:: Mesa(GameState* g, Vector2D<> pos, Texture* tex,int i,int width,int height,Game* game) : sceneObject(g, pos, tex), hover(false), texture(tex),indexGame(i)
+{
+	_game = game;
+	w = width;
+	h = height;
+}
+
+
+void Mesa::render() const {
+	SDL_Rect rect = getRenderRect();
+	if (!hover) {
+		texture->render(rect);
+	}
+	else {
+		texture->render(rect, SDL_Color(255, 255, 0));
+	}
+ }
+void Mesa:: update(){}
+
+void Mesa::handleEvent(SDL_Event const& evento) {
+	if (evento.key.keysym.sym == SDLK_e && hover) {
+		_game->push(comportamiento.loadMadness(indexGame, _game));
+	}
+}
+
+Collision Mesa::hit(const SDL_Rect& rect, Collision::Target target) {
+	return NO_COLLISION;
+}
+SDL_Rect Mesa::getRect() const {
+	SDL_Rect rect;
+	rect.x = pos.getX();
+	rect.y = pos.getY() - h;
+	rect.w = w;//ancho
+	rect.h = h;//alto
+	return rect;
+}
