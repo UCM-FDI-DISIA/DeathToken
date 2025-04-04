d49cd26f9efb39a0f326a997d6adb6e2e572ba20
Ismaorte <ismaorte@ucm.es>
2025-04-04 15:17:03 +0200

    Revert "Merge branch 'Baccarat' into Físicas"
    This reverts commit 47cac22de5ab2aabb1e72ba99d5cc1b81314b87c, reversing
    changes made to 1165b2d2b9af088f9e33fc946f045819ca0559e3.

--- DeathTokenBase/DeathToken/BaccaratBet.h
+++ DeathTokenBase/DeathToken/BaccaratBet.h
@@ -2,14 +2,8 @@
 #include "Baccarat.h"
 class BaccaratBet : public Baccarat
 {
-	Button* bet = nullptr;
-	Button* stop = nullptr;
-	bool buttonsOn = false;
 public:
 	BaccaratBet(Game* game) : Baccarat(game) {};
-	void acumulate();
-	void repeatBet();
-	void update() override;
-	void didntWin();
+	void repeat() override;
 };
 
