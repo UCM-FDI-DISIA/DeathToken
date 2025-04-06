12ac6e6dc1e3c5aea6faaa81ce7e2d74339981d2
Janime555 <157594411+Janime-UCM555@users.noreply.github.com>
2025-04-06 17:20:38 +0200

    arreglos para el merge

--- DeathTokenBase/DeathToken/sceneObject.h
+++ DeathTokenBase/DeathToken/sceneObject.h
@@ -17,15 +17,15 @@ protected:
 	int w, h;
 	int moveDelay;
 	GameState* state;
-	GameList<sceneObject>::anchor anchor;
 
 public:
+	sceneObject(GameState*, Vector2D<>, int, int, Texture*);
 	sceneObject(GameState*, istream&, Vector2D<>, int, int, Texture*);
 	sceneObject(GameState*, Vector2D<>, Vector2D<>, int, int, Texture*);
 	sceneObject(GameState*, Texture*);
 	sceneObject(GameState*, Vector2D<>, Texture*);
-	virtual ~sceneObject() { texture = nullptr; state = nullptr; };
-	void setListAnchor(GameList<sceneObject>::anchor&& anchor);
+
+	virtual ~sceneObject() { texture = nullptr; state = nullptr; }
 	virtual Collision hit(const SDL_Rect&, Collision::Target) = 0;
 	Vector2D<> position() const { return pos; };
 	Collision tryToMove(const Vector2D<>&, Collision::Target);
